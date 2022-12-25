cfbaf73e611c65c4d1ce1830c1df94c2f0e42c3e
KimReachna <kimreachna@gmail.com>
2022-12-25 15:05:23 +0300

    ReactNative Lab

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [labelText, setLabelText] = useState("Нажмите для получения")
+
+  function click(){
+
+    setLabelText(new Date().toDateString())
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Текущая дата:</Text>
+      <Text>{labelText}</Text>
+      <Button title='получить дату' onPress={click}></Button>
     </View>
   );
 }
